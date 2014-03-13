fc2bde5707a3fbb8bcd41dfbdc5902b1d0a61706
Gabriel Mateus <contato@gabrielmateus.com>
2014-03-13 19:05:54 -0300

    Fix the gradient mixin in to repo status

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,12 +1,16 @@
+// Get Json
 (function ($) {
-
-  // var jsonURl = 'http://localhost:5000/statuses.json';
-  var jsonURl = 'drone.json';
+  var jsonURl = 'http://localhost:5000/statuses.json';
+  var template = $.trim( $('#template').html() );
+  var frag = '';
 
   $.getJSON(jsonURl,function(data){
-
-    $.each(data, function(i, field){
-      $("#container").append(i, field);
+    $.each(data.repositories, function(i, obj){
+      frag += template.replace( /{{ name }}/ig, obj.name ).replace( /{{ status }}/ig, obj.status );
     });
+    $("#repositories-list").append(frag);
+  });
+
+  $("#template").remove();
 
 })(jQuery);
